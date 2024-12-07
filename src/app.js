2031764c257dd0ddb1f363acda5692833034fe93
Your Name <lakshmijaya2912@gmail.com>
2024-12-07 11:13:35 +0530

    pendings covered

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ initializeSocket(io);
 
 // app.use(morgan('dev')); 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", `${config.API_URL}`);
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
